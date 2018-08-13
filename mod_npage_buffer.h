40fb82585c74eaa9520495d4d3fc7f8f32135842
Benedikt Schmied <b.schmied@gmx.com>
2018-08-13 23:15:05 +0200

    initial check in

---
+++ mod_npage_buffer.h
@@ -0,0 +1,152 @@
+/**
+ * \addtogroup mod_npage_buffer_gnc
+ * \{
+ * \file mod_npage_buffer_gnc.h
+ *
+ */
+
+#ifndef __mod_npage_buffer_gnc_H__
+#define __mod_npage_buffer_gnc_H__
+
+////////////////////////////////////////////////////////////////////////////////
+/// Includes
+////////////////////////////////////////////////////////////////////////////////
+
+/* c - runtime */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/* system */
+
+/* own libs */
+
+/* project */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+/// Macros
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// type definitions, unions, structures
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// interface function
+////////////////////////////////////////////////////////////////////////////////
+
+/**************************************************************************//**
+ * @brief  mod_npage_buffer_gnc__init
+ *
+ * @param [in,out]   _p         description
+ * @return     0, if successful or < 0, if failed
+ *             + '-1', initialization has failed
+ * ****************************************************************************/
+int mod_npage_buffer_gnc__init(void *_p);
+
+/**************************************************************************//**
+ * @brief  mod_npage_buffer_gnc__cleanup
+ *
+ * @param [in,out]   _p         description
+ * @return     0, if successful or < 0, if failed
+ *             + '-1', initialization has failed
+ * ****************************************************************************/
+int mod_npage_buffer_gnc__cleanup(void *_p);
+
+/**************************************************************************//**
+ * @brief  mod_npage_buffer_gnc__reinit
+ *
+ * @param [in,out]   _p         description
+ * @return     0, if successful or < 0, if failed
+ *             + '-1', initialization has failed
+ * ****************************************************************************/
+int mod_npage_buffer_gnc__reinit(void *_p);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/** \} */
+/**
+ * \addtogroup mod_npage_buffer_gnc
+ * \{
+ * \file mod_npage_buffer_gnc.h
+ *
+ */
+
+#ifndef __mod_npage_buffer_gnc_H__
+#define __mod_npage_buffer_gnc_H__
+
+////////////////////////////////////////////////////////////////////////////////
+/// Includes
+////////////////////////////////////////////////////////////////////////////////
+
+/* c - runtime */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/* system */
+
+/* own libs */
+
+/* project */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+/// Macros
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// type definitions, unions, structures
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// interface function
+////////////////////////////////////////////////////////////////////////////////
+
+/**************************************************************************//**
+ * @brief  mod_npage_buffer_gnc__init
+ *
+ * @param [in,out]   _p         description
+ * @return     0, if successful or < 0, if failed
+ *             + '-1', initialization has failed
+ * ****************************************************************************/
+int mod_npage_buffer_gnc__init(void *_p);
+
+/**************************************************************************//**
+ * @brief  mod_npage_buffer_gnc__cleanup
+ *
+ * @param [in,out]   _p         description
+ * @return     0, if successful or < 0, if failed
+ *             + '-1', initialization has failed
+ * ****************************************************************************/
+int mod_npage_buffer_gnc__cleanup(void *_p);
+
+/**************************************************************************//**
+ * @brief  mod_npage_buffer_gnc__reinit
+ *
+ * @param [in,out]   _p         description
+ * @return     0, if successful or < 0, if failed
+ *             + '-1', initialization has failed
+ * ****************************************************************************/
+int mod_npage_buffer_gnc__reinit(void *_p);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/** \} */
